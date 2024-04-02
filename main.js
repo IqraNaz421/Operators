a5c50190bb7face0a4433c74ffd94bf59ce9e3ea
Iqra Naz <161621730+IqraNaz421@users.noreply.github.com>
2024-04-03 01:43:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//class 4 Assignment (operators)
+//Arithmetic operators:
+//addition
+var num1 = 10;
+var num2 = 5;
+var sum = (num1 + num2);
+console.log(sum);
+//subtraction
+var a = 15;
+var b = 3;
+var difference = (a - b);
+console.log(difference);
+//multiplication
+var c = 6;
+var d = 6;
+var mul = (c * d);
+console.log(mul);
+//division
+var students = 20;
+var pencils = 10;
+var div = (students / pencils);
+console.log(div);
+//exponentiation
+var ab = 4;
+var cd = 3;
+var exp = (Math.pow(ab, cd));
+console.log(exp);
+//modulus
+var toffee = 5;
+var child = 3;
+var mod = (toffee % child);
+console.log(mod);
---
+++ main.ts
@@ -0,0 +1,38 @@
+//class 4 Assignment (operators)
+//Arithmetic operators:
+
+//addition
+let num1: number = 10
+let num2: number = 5
+let sum = (num1+num2)
+console.log(sum);
+
+//subtraction
+let a: number = 15
+let b: number = 3
+let difference = (a-b)
+console.log(difference);
+
+//multiplication
+let c: number = 6
+let d: number = 6
+let mul = (c*d)
+console.log(mul);
+
+//division
+let students: number = 20
+let pencils: number = 10
+let div = (students/pencils)
+console.log(div);
+
+//exponentiation
+let ab: number = 4
+let cd: number = 3
+let exp = (ab**cd)
+console.log(exp);
+
+//modulus
+let toffee: number = 5
+let child: number = 3
+let mod = (toffee%child)
+console.log(mod);
